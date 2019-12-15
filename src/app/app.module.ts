5b2b8b94372e6cc0ba244ea08ac8cb822f1a87a4
domovikx <domovikx@gmail.com>
2019-12-15 20:10:43 +0300

    chore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './modules/navigation/navigation.component';
+
 import { PageAboutModule } from './pages/page-about/page-about.module';
 import { PageHomeModule } from './pages/page-home/page-home.module';
 import { PageArticlesModule } from './pages/page-articles/page-articles.module';
@@ -13,8 +13,11 @@ import { PageClientsModule } from './pages/page-clients/page-clients.module';
 import { PageContactsModule } from './pages/page-contacts/page-contacts.module';
 import { PageSupportModule } from './pages/page-support/page-support.module';
 
+import { FooterComponent } from './modules/footer/footer.component';
+import { HeaderComponent } from './modules/header/header.component';
+
 @NgModule({
-  declarations: [AppComponent, NavigationComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
