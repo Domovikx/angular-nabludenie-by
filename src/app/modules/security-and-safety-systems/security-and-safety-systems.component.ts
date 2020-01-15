b336443a276ec2922dfa7a691fa9408d7015c2fe
domovikx <domovikx@gmail.com>
2020-01-16 00:47:40 +0300

    feat: add security-and-safety-systems component

---
+++ src/app/modules/security-and-safety-systems/security-and-safety-systems.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-security-and-safety-systems',
+  templateUrl: './security-and-safety-systems.component.html',
+  styleUrls: ['./security-and-safety-systems.component.scss']
+})
+export class SecurityAndSafetySystemsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/pages/page-home/page-home.module.ts
+++ src/app/pages/page-home/page-home.module.ts
@@ -6,9 +6,16 @@ import { SliderSmModule } from 'src/app/modules/slider-sm/slider-sm.module';
 import { AboutCompanyModule } from 'src/app/modules/about-company/about-company.module';
 
 import { PageHomeComponent } from './page-home.component';
+import { SecurityAndSafetySystemsModule } from 'src/app/modules/security-and-safety-systems/security-and-safety-systems.module';
 
 @NgModule({
-  imports: [CommonModule, BrandsSmModule, SliderSmModule, AboutCompanyModule],
+  imports: [
+    CommonModule,
+    BrandsSmModule,
+    SliderSmModule,
+    AboutCompanyModule,
+    SecurityAndSafetySystemsModule
+  ],
   declarations: [PageHomeComponent]
 })
 export class PageHomeModule {}
