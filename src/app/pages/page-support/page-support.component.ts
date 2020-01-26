3de5a5d8ae7577d8170424b1a47b34769c6abe3c
domovikx <domovikx@gmail.com>
2020-01-26 03:17:50 +0300

    chore

--- src/app/pages/page-support/page-support.component.ts
+++ src/app/pages/page-support/page-support.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-support.component.scss']
 })
 export class PageSupportComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
