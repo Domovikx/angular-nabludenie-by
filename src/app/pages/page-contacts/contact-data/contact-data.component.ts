1fb8a9a0616567e3e0bc40759a64f5633258cf79
domovikx <domovikx@gmail.com>
2020-02-03 02:17:03 +0300

    chore

---
+++ src/app/pages/page-contacts/contact-data/contact-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-data',
+  templateUrl: './contact-data.component.html',
+  styleUrls: ['./contact-data.component.scss']
+})
+export class ContactDataComponent {
+  constructor() {
+    //
+  }
+}
--- src/app/pages/page-contacts/page-contacts.module.ts
+++ src/app/pages/page-contacts/page-contacts.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PageContactsComponent } from './page-contacts.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MapYandexModule } from 'src/app/modules/map-yandex/map-yandex.module';
+import { PageContactsComponent } from './page-contacts.component';
+import { ContactDataComponent } from './contact-data/contact-data.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
 
 @NgModule({
-  imports: [CommonModule, MapYandexModule],
-  declarations: [PageContactsComponent]
+  imports: [CommonModule, MapYandexModule, FormsModule, ReactiveFormsModule],
+  declarations: [PageContactsComponent, ContactDataComponent, QuestionFormComponent]
 })
 export class PageContactsModule {}
